e44ae3f3d9b552527ba8fe8c3cb05e52746358f2
koponirov <koponirov92@gmail.com>
2020-12-16 12:58:38 +0700

    finished Message component

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,24 +1,51 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Button as BaseButton} from 'antd'
 import classNames from 'classnames'
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+import checkedSvg from '../../assets/icons/checked.svg'
 
 import './Message.scss'
 
-const Message = ({avatar, text, date }) => {
+const Message = ({avatar, text, date, isMe, user, isReaded }) => {
     return (
-        <div className='message'>
-            <div className='message__avatar'>
-                <img src={avatar} alt='User avatar'/>
+        <div className={classNames("message", {"message--isme":isMe })}>
+            <div className="message__content">
+                {isMe && isReaded && (
+                    <img
+                        className="message__icon-readed"
+                        src={checkedSvg}
+                        alt="checked icon" />
+                )
 
+                }
+
+                <div className='message__avatar'>
+                    <img src={avatar} alt={`Avatar ${user.fullname}`}/>
+                </div>
+                <div className="message__info">
+                    <div className="message__bubble">
+                        <p className="message__text">{text}</p>
+                    </div>
+                    <span className="message__date">
+                        {formatDistanceToNow(date, { addSuffix: true })}
+                    </span>
+
+                </div>
             </div>
+
         </div>
     )
 }
 
+Message.defaultProps = {
+    user: {}
+};
+
 Message.propTypes = {
     className: PropTypes.string,
-    avatar: PropTypes.string
+    avatar: PropTypes.string,
+    text: PropTypes.string,
+    date: PropTypes.string,
 }
 
 export default Message;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -11,12 +11,14 @@ const Home = () => (
         <Message
             avatar='https://sun4-17.userapi.com/impg/lwpDTTfQhgyydEp-9LdFHElFpG_CWPL0EbvNHg/pr93lesNDqA.jpg?size=100x0&quality=96&crop=142,0,717,717&sign=2e71adb148b61e1c417c68a8eaa744af&ava=1'
             text='Привет! Вот мое сообщение для тебя!'
-            date=''
+            date={new Date}
+            isMe={true}
+            isReaded={true}
         />
         <Message
             avatar='https://sun4-10.userapi.com/impf/c830408/v830408279/146eb8/NBu4Zr9cByg.jpg?size=100x0&quality=96&crop=65,0,587,587&sign=910f381d499697417b53c055514a1033&ava=1'
             text='Привет, сообщение как сообщение'
-            date=''
+            date={new Date}
         />
 
     </section>
