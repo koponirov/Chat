e796d939294a900b09e99514c4ad76ca927c7199
koponirov <koponirov92@gmail.com>
2020-12-17 12:05:27 +0700

    added attachments to Message component

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -2,33 +2,48 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import formatDistanceToNow from 'date-fns/formatDistanceToNow'
-import checkedSvg from '../../assets/icons/checked.svg'
+import readedSvg from '../../assets/icons/readed.svg'
+import notReadedSvg from '../../assets/icons/not-readed.svg'
 
 import './Message.scss'
 
-const Message = ({avatar, text, date, isMe, user, isReaded }) => {
+const Message = ({avatar, text, date, isMe, user, isReaded, attachments}) => {
     return (
-        <div className={classNames("message", {"message--isme":isMe })}>
+        <div className={classNames("message", {"message--isme": isMe})}>
             <div className="message__content">
-                {isMe && isReaded && (
+                {isMe && isReaded ? (
                     <img
                         className="message__icon-readed"
-                        src={checkedSvg}
-                        alt="checked icon" />
-                )
-
-                }
+                        src={readedSvg}
+                        alt="readed icon"/>
+                ) : (
+                    <img
+                        className="message__icon-readed"
+                        src={notReadedSvg}
+                        alt="not readed icon"/>
+                )}
 
                 <div className='message__avatar'>
                     <img src={avatar} alt={`Avatar ${user.fullname}`}/>
                 </div>
                 <div className="message__info">
+
                     <div className="message__bubble">
                         <p className="message__text">{text}</p>
                     </div>
+                    <div className="message__attachments">
+                        {
+                            attachments && attachments.map(item => (
+                                <div className="message__attachments-item">
+                                    <img src={item.url} alt={item.filename}/>
+                                </div>
+                            ))
+                        }
+                    </div>
                     <span className="message__date">
-                        {formatDistanceToNow(date, { addSuffix: true })}
-                    </span>
+                        {formatDistanceToNow(date, {addSuffix: true})}
+                        </span>
+
 
                 </div>
             </div>
@@ -46,6 +61,8 @@ Message.propTypes = {
     avatar: PropTypes.string,
     text: PropTypes.string,
     date: PropTypes.string,
+    isReaded: PropTypes.bool,
+    attachments: PropTypes.array,
 }
 
 export default Message;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -19,6 +19,32 @@ const Home = () => (
             avatar='https://sun4-10.userapi.com/impf/c830408/v830408279/146eb8/NBu4Zr9cByg.jpg?size=100x0&quality=96&crop=65,0,587,587&sign=910f381d499697417b53c055514a1033&ava=1'
             text='Привет, сообщение как сообщение'
             date={new Date}
+            attachments={[
+                {
+                    filename:'image.jpg',
+                    url: 'https://source.unsplash.com/50x50/?random=1&nature,water'
+                },
+                {
+                    filename:'image.jpg',
+                    url: 'https://source.unsplash.com/50x50/?random=2&nature,water'
+                },
+                {
+                    filename:'image.jpg',
+                    url: 'https://source.unsplash.com/50x50/?random=3&nature,water'
+                }
+            ]}
+        />
+        <Message
+            avatar='https://sun4-17.userapi.com/impg/lwpDTTfQhgyydEp-9LdFHElFpG_CWPL0EbvNHg/pr93lesNDqA.jpg?size=100x0&quality=96&crop=142,0,717,717&sign=2e71adb148b61e1c417c68a8eaa744af&ava=1'
+            text='Привет! Вот мое сообщение для тебя!'
+            date={new Date}
+            isMe={true}
+            isReaded={false}
+        />
+        <Message
+            avatar='https://sun4-10.userapi.com/impf/c830408/v830408279/146eb8/NBu4Zr9cByg.jpg?size=100x0&quality=96&crop=65,0,587,587&sign=910f381d499697417b53c055514a1033&ava=1'
+            text='Привет, сообщение как сообщение'
+            date={new Date}
         />
 
     </section>
